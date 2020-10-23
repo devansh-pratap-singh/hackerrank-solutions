617a510049af0e7236b0791e620ee26970dd0259
devansh-pratap-singh <devansh.pratap@gmail.com>
2020-10-23 16:00:47 +0530

    Day 8: Create A Button

---
+++ 10 Days of JS/8CreateAButton.js
@@ -0,0 +1,27 @@
+// HTML
+// <!DOCTYPE HTML>
+// <html>
+//     <head>
+//         <meta charset="utf-8">
+//         <title>Button</title>
+//         <link rel="stylesheet" href="css/button.css" type="text/css">
+//     </head>
+
+// 			<body>
+// 				<button id="btn">0</button>
+//         <script src="js/button.js" type="text/javascript"></script>
+//     </body>
+// </html>
+
+// CSS
+// #btn {
+//     width: 96px;
+//     height: 48px;
+//     font-size: 24px;
+// }
+
+// JavaScript
+var btn = document.getElementById("btn");
+btn.addEventListener("click", function () {
+	btn.innerHTML++;
+});
