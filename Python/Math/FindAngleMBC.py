181b9568685c07f2976b0c043729da2f1e1f6135
devansh-pratap-singh <devansh.pratap@gmail.com>
2020-10-31 11:20:07 +0530

    Python Update

---
+++ Python/Math/FindAngleMBC.py
@@ -0,0 +1,4 @@
+import math
+ab = float(input())
+bc = float(input())
+print(str(int(round(math.degrees(math.atan2(ab, bc))))) + '°')
---
+++ Python/Math/ModDivmod.py
@@ -0,0 +1,6 @@
+import math
+A = int(input())
+B = int(input())
+print(math.floor(A/B))
+print(A%B)
+print(divmod(A,B))
