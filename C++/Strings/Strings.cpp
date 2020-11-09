2bb5010a331d18e07c32068294a6147918790bb8
devansh-pratap-singh <devansh.pratap@gmail.com>
2020-11-09 17:37:57 +0530

    C++ Update

---
+++ C++/Strings/Strings.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string a,b;
+  cin>>a>>b;
+  cout<<a.size()<<" "<<b.size()<<endl<<a+b;
+  char temp = a[0];
+  a[0] = b[0];
+  b[0] = temp;
+  cout<<endl<<a<<" "<<b;
+  return 0;
+}
