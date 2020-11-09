15b16fa9bb0baf5072b80c247269cff32e79b19d
devansh-pratap-singh <devansh.pratap@gmail.com>
2020-11-09 17:50:32 +0530

    C++ Update

---
+++ C++/Introduction/BasicDataTypes.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdio>
+#include <iomanip>
+using namespace std;
+
+int main() {
+  int i; long l; char c; float f; double d;
+  cin>>i>>l>>c>>f>>d;
+  cout<<setprecision(20)<<i<<endl<<l<<endl<<c<<endl<<f<<endl<<d<<endl;
+  return 0;
+}
